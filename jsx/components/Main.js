094ab5512382ff9ae66e267eb3caea45e53e2933
tigershen23 <tigershen23@gmail.com>
2015-10-16 11:42:34 -0700

    Add API helpers and get repos from Github on Main handleSubmit

--- jsx/components/Main.js
+++ jsx/components/Main.js
@@ -73,6 +73,26 @@ class Main extends React.Component {
     this.setState({
       isLoading: true
     });
+    api.getBio(this.state.username)
+      .then((res) => {
+        if (res.message === "Not Found") {
+          this.setState({
+            error: "User not found",
+            isLoading: false,
+          });
+        } else {
+          this.props.navigator.push({
+            title: res.name || "Select an Option",
+            component: Dashboard,
+            passProps: {userInfo: res}
+          })
+          this.setState({
+            isLoading: false,
+            error: false,
+            username: "",
+          });
+        }
+      })
 
     console.log("SUBMITTED", this.state.username);
   }
