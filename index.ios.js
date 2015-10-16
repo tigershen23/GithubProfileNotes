219d0478d7166fc9f0fa0666344202b6998c49ac
tigershen23 <tigershen23@gmail.com>
2015-10-16 11:07:11 -0700

    Add first component and routing with NavigatorIOS

--- index.ios.js
+++ index.ios.js
@@ -5,28 +5,24 @@
 'use strict';
 
 var React = require('react-native');
+var Main = require("./jsx/components/Main")
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View,
 } = React;
 
 class GithubProfileNotes extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Github Profile Notes!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: "Github Profile Notes",
+          component: Main
+        }} />
     );
   }
 };
@@ -34,19 +30,7 @@ class GithubProfileNotes extends React.Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#111111',
   },
 });
 
